3b8279ec3ccaf65ecb326e3642bba45f7eaa1e7a
Daxuan <Kimren227@hotmail.com>
2016-09-16 08:53:09 +0800

    Update RPIToArduino.py

--- RPIToArduino.py
+++ RPIToArduino.py
@@ -3,4 +3,7 @@
 
 import serial
 ser = serial.Serial('/dev/ttyACM0',9600);
-serial.write('123');
+# serial.write take a input of 4 digit string 
+# first digit 0 is go forward 1 is backward 2 is Counter-Clock rotate 3 is clockwise
+# other three digit is function dependent parameter
+serial.write('1234');
